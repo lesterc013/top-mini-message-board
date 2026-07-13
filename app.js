867ad9dbbc3b2b5acf407b6b51acb1c0d45a8fa4
lester-mtse-machine <lesterc013@gmail.com>
2026-07-13 17:09:35 +0800

    Setup app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import path from 'node:path';
+
+import indexRouter from './src/routes/indexRouter.js';
+
+const app = express();
+const PORT = 3000;
+
+// Set up to render in ejs
+app.set('views', path.join(import.meta.dirname, 'src/views'));
+app.set('view engine', 'ejs');
+
+app.use('/', indexRouter);
+
+app.listen(PORT, (err) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  console.log(`Listening on ${PORT}`);
+});
