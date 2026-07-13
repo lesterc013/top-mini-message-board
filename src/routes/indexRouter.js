e30565a73c1948f6216c2bd3eb17a0357c6922b3
lester-mtse-machine <lesterc013@gmail.com>
2026-07-13 17:29:23 +0800

    Add /new functionality to indexRouter

--- src/routes/indexRouter.js
+++ src/routes/indexRouter.js
@@ -19,4 +19,18 @@ router.get('/', (req, res) => {
   res.render('index', { messages });
 });
 
+router.get('/new', (req, res) => {
+  res.render('form');
+});
+
+router.post('/new', (req, res) => {
+  messages.push({
+    text: req.body.text,
+    user: req.body.user,
+    added: new Date(),
+  });
+
+  res.redirect('/');
+});
+
 export default router;
